477d1dc05507afed7b40e1eab2027e7ad447323a
Aiman Almureish <aiman.almureish@gmail.com>
2019-11-14 19:53:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your Circle class here
+
+
+class Circle {
+  constructor(radius){
+    this.radius = radius;
+  }
+  
+  get diameter(){
+    return 2 * this.radius
+  }
+  
+    get circumference(){
+        return 2 * Math.PI() * this.radius
+
+  }
+  
+    get area(){
+    
+  }
+}
